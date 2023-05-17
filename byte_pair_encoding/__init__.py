5a7ac8ae37e387bb6baaf0a4238b0ac15b087f5e
Alok Singh <alokbeniwal@gmail.com>
2023-05-16 17:53:08 -0700

    move bpe into functions

--- byte_pair_encoding/__init__.py
+++ byte_pair_encoding/__init__.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass, replace
 from typing import Sequence, Mapping
 import urllib.request
 import urllib.response
+
 # %%
 ALPHABET = string.ascii_letters
 SAMPLE_TEXT = "aaabdaaabac"
@@ -22,78 +23,89 @@ not_in_sample = set(ALPHABET) - set(SAMPLE_TEXT)  # alphabet minus sampletext me
 text = SAMPLE_TEXT
 luc_dict = {}
 
+# # Megan's attempt
 
+# for _ in range(DEPTH):
+#     splt = [text[i : i + CODON] for i in range(0, len(text), CODON)]
+#     # get frequency table? (intuition that this probably isn't especially efficient)
+#     frequency = collections.Counter(
+#         splt
+#     ).most_common()  # ...seems to auto spit out most common first? TODO:doublecheck this
+#     while True:
+#         p, n = frequency.popitem()
+#         print(f{'p='})
+#         if n < MIN_FREQ:
+#             break
 
-for _ in range(DEPTH):
-    splt = [text[i : i + CODON] for i in range(0, len(text), CODON)]
-    # get frequency table? (intuition that this probably isn't especially efficient)
-    frequency = collections.Counter(
-        splt
-    ).most_common()  # ...seems to auto spit out most common first? TODO:doublecheck this
-    while True:
-        p, n = frequency.popitem()
-        print(f{'p='})
-        if n < MIN_FREQ:
-            break
+#         s = not_in_sample.pop()
+#         print(f'f{p,s=}')
+#         luc_dict.update({s: p})
+#         text = text.replace(p, s)
 
-        s = not_in_sample.pop()
-        print(f'f{p,s=}')
-        luc_dict.update({s: p})
-        text = text.replace(p, s)
+# print(text)
+# print(luc_dict)
 
-print(text)
-print(luc_dict)
-
-# %%
-# ## pseudocode
-# text2 = SAMPLE_TEXT
-# lut_dict = dict()
-#
-# for all common_pairs in SAMPLE_TEXT:
-#     add to lut_dict
-#     replace in text_2
-#
-# for all common_pairs in text_2:
-#     add to lut_dict
-#     replace in text_2
-#
-# ## todo: set up so you can use a variable to set depth of recursion
-# ## (note that in this implementation, you also need to know depth to de-convert; {X:ab, Y:bc, Z:XY})
+# # %%
+# # ## pseudocode
+# # text2 = SAMPLE_TEXT
+# # lut_dict = dict()
+# #
+# # for all common_pairs in SAMPLE_TEXT:
+# #     add to lut_dict
+# #     replace in text_2
+# #
+# # for all common_pairs in text_2:
+# #     add to lut_dict
+# #     replace in text_2
+# #
+# # ## todo: set up so you can use a variable to set depth of recursion
+# # ## (note that in this implementation, you also need to know depth to de-convert; {X:ab, Y:bc, Z:XY})
 
 # %%
 sample = SAMPLE_TEXT
 
 
-
-
-def bpe(s:str)->str,list[bidict.bidict]:...
-
+def bpe(s: str) -> tuple[str, list[bidict.bidict]]:
     # TODO replace string.punctuation with more general thing
     # replacements=dict(zip(pairs,collections.deque(string.punctuation)))
     replacements: list[bidict.bidict] = []
-    REPLACEMENT_CHAR_SET = set(zhon.cedict.traditional)
+    replacement_char_set = set(zhon.cedict.traditional)
     while True:
         pairs = collections.Counter(itertools.pairwise(s))
-        REPLACEMENT_CHARS = []
+        replacement_chars = []
         for _ in range(len(pairs)):
-            REPLACEMENT_CHARS.append(REPLACEMENT_CHAR_SET.pop())
+            replacement_chars.append(replacement_char_set.pop())
 
-        replacement = bidict.bidict(zip((p for p,ct in pairs.items() if ct>1), REPLACEMENT_CHARS))
-        #print(replacement)
+        replacement = bidict.bidict(
+            zip(
+                (p for p, ct in pairs.items() if ct > 1),
+                replacement_chars,
+            )
+        )
         replacements.append(replacement)
-        new_str = s
+
+        new = s
         for pair, new_char in replacement.items():
-            #print(f"{pair=}")
-            new_sample = new_sample.replace("".join(pair), new_char)
-        print(f"{sample} -> {new_sample}")
-        if len(new_sample) < len(sample):
-            sample = new_sample
+            new = new.replace("".join(pair), new_char)
+        print(f"{s} -> {new}")
+        if len(new) < len(s):
+            s = new
         else:
             break
+    return s, replacements
 
 
+def rev_bpe(s: str, replacements: list[bidict.bidict]) -> str:
+    # rev
+    new = s
+    for replacement in reversed(replacements):
+        for new_char, pair in replacement.inv.items():
+            new = new.replace(new_char, "".join(pair))
+            print(f"{new}")
+    return new
 
-def rev_bpe(s:str,replacements:list[bidict.bidict])->str:...
+
+assert (rev_bpe(*bpe(SAMPLE_TEXT))) == SAMPLE_TEXT
 
 # TODO replace string.punctuation with more general thing
 # replacements=dict(zip(pairs,collections.deque(string.punctuation)))
@@ -105,12 +117,14 @@ while True:
     for _ in range(len(pairs)):
         REPLACEMENT_CHARS.append(REPLACEMENT_CHAR_SET.pop())
 
-    replacement = bidict.bidict(zip((p for p,ct in pairs.items() if ct>1), REPLACEMENT_CHARS))
-    #print(replacement)
+    replacement = bidict.bidict(
+        zip((p for p, ct in pairs.items() if ct > 1), REPLACEMENT_CHARS)
+    )
+    # print(replacement)
     replacements.append(replacement)
     new_sample = sample
     for pair, new_char in replacement.items():
-        #print(f"{pair=}")
+        # print(f"{pair=}")
         new_sample = new_sample.replace("".join(pair), new_char)
     print(f"{sample} -> {new_sample}")
     if len(new_sample) < len(sample):
@@ -127,11 +141,13 @@ for replacement in reversed(replacements):
         print(f"{new_sample}")
 
 newest_sample = new_sample
-print(f'{newest_sample, SAMPLE_TEXT = }')
+print(f"{newest_sample, SAMPLE_TEXT = }")
 assert newest_sample == SAMPLE_TEXT
 # %%
 
-with urllib.request.urlopen('https://en.wikipedia.org/wiki/Byte_pair_encoding') as response:
-   html = response.read().decode('utf-8')
-   print(html)
+with urllib.request.urlopen(
+    "https://en.wikipedia.org/wiki/Byte_pair_encoding"
+) as response:
+    html = response.read().decode("utf-8")
+    print(html)
 # %%
